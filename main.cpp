21568ee308f7d85376dbf76cddc72ed9297464c2
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-05-10 13:17:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,20 +20,23 @@ int main() {
     auto* ataque2 = new Firearm();
     auto* ataque3 = new Saber();
     auto* ataque4 = new Saber();
-    auto* defensa3 = new Shield();
+    auto* defensa3 = new Elude();
     auto* defensa4= new Armor();
     b->add_attack(ataque4);
     b->add_defense(defensa3);
     a->add_defense(defensa4);
     a->add_attack(ataque1);
-    a->add_attack(ataque3);
-    b->add_attack(ataque2);
     a->add_defense(defensa1);
     b->add_defense(defensa2);
     Fight* pelea = new Fight(a,b);
     Arena* arena = new Arena;
-    arena->run(pelea);
+    assert(pelea->get_score() == 0);
+    a->add_attack(ataque3);
+    b->add_attack(ataque2);
+    pelea = new Fight(a,b);
     assert(defensa1->defense()==1);
     assert(ataque3->attack() == 5);
-    assert(pelea->get_score() == -8);
+    //cuando tienen dos defensas y dos atacques cada uno
+    assert(pelea->get_score() == -5);
+    assert(pelea->get_winner() == pelea->get_peleador2());
 }
