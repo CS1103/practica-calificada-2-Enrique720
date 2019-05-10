f9603a18b4b6766643223f46e8ad0d77936211e3
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-05-10 13:18:13 -0500

    Update Fight.h

--- Fight.h
+++ Fight.h
@@ -1,6 +1,3 @@
-//
-// Created by enrique.sobrados on 10/05/2019.
-//
 
 #ifndef EXAMEN_FIGHT_H
 #define EXAMEN_FIGHT_H
@@ -18,7 +15,10 @@ private:
 public:
     Fight( Fighter* peleador1, Fighter* peleador2);
     int get_score();
-    string get_winner();
+    string get_winner_name();
+    Fighter* get_peleador1();
+    Fighter* get_peleador2();
+    Fighter* get_winner();
 };
 
 
